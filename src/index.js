db9d0a87843e05c4332c5176435f13df1fe22044
Jeremias0901 <cuellojeremiasnatanael@gmail.com>
2023-10-06 20:49:07 -0300

    configuracion de env - manejo de erroes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import express from 'express';
-import employeesRoutes from "./routes/employee.routes.js";
-import indexRoutes from "./routes/index.routes.js";
+import app from "./app.js";
+import { PORT } from './config.js'
 
-const app = express();
-const port = 3000;
-
-app.use(express.json())
-
-app.use(indexRoutes);
-app.use('/api', employeesRoutes);
-
-app.listen(port)
-console.log('Eschuchando en http://localhost:' + port);
+app.listen(PORT)
+console.log('Eschuchando en http://localhost:' + PORT);
